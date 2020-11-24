48ca7d0b2d7a0fe018e22e225e760842048fc59f
FearlessBBH <452371660@qq.com>
2020-11-24 17:39:45 +0800

    第五课课中

---
+++ [20201124]Vue-5/app/src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="app">
+    <h1>HELLO VUE!!!!</h1>
+    <button @click='x++'>{{x+y}}</button>
+    <button @click='isShow= !isShow'>{{isShow}}</button>
+    <hr />
+    <hello-world v-if='isShow' :data='x+y'></hello-world>
+  </div>
+</template>
+
+<script>
+// 导出来的是一个对象，不是组件
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      x: 1,
+      y: 2,
+      isShow: false
+    }
+  },
+  components: {
+    HelloWorld
+  },
+  beforeCreate() {
+    console.group('beforeCreate');
+    console.log(this.$data);
+    console.groupEnd('beforeCreate');
+  },
+  created() {
+    console.group('created');
+    console.log(this.$data);
+    console.log(this.$el);
+    console.groupEnd('created');
+  },
+  beforeMount() {
+    console.group('beforeMount');
+    console.log(this.$el);
+    console.groupEnd('beforeMount');
+  },
+  mounted() {
+    console.group('mounted');
+    console.log(this.$el);
+    console.groupEnd('mounted');
+  },
+  beforeUpdate() {
+    console.group('beforeUpdate');
+    console.groupEnd('beforeUpdate');
+  },
+  updated() {
+    console.group('updated');
+    console.groupEnd('updated');
+  },
+  errorCaptured() {
+    console.log('子组件出错了')
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
